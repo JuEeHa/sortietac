77384eaaff82328652f2f50e001326c6bb064e54
Juhani Haverinen <juhani.haverinen@gmail.com>
2013-08-29 23:29:49 +0300

    First commit

---
+++ sortied.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+echo ':j #osdev-offtopic'
+while true
+do
+	MAXLEN=$(grep -c '^..:.. <.sortiecat>' ~/irclogs/freenode/#osdev-offtopic.log)
+	grep '^..:.. <.sortiecat>' ~/irclogs/freenode/#osdev-offtopic.log | tail -n $(($RANDOM%$MAXLEN)) | head -n 1 | cut -d '>' -f 2- | cut -d ' ' -f 2-
+	sleep 20
+done
