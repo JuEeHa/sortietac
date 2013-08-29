6cf01ddda1788376686f2cb97d87dfc8f516fa5d
Juhani Haverinen <juhani.haverinen@gmail.com>
2013-08-29 23:44:15 +0300

    Changed delay to random

--- sortied.sh
+++ sortied.sh
@@ -5,5 +5,5 @@ while true
 do
 	MAXLEN=$(grep -c '^..:.. <.sortiecat>' ~/irclogs/freenode/#osdev-offtopic.log)
 	grep '^..:.. <.sortiecat>' ~/irclogs/freenode/#osdev-offtopic.log | tail -n $(($RANDOM%$MAXLEN)) | head -n 1 | cut -d '>' -f 2- | cut -d ' ' -f 2-
-	sleep 20
+	sleep $(($RANDOM%(5*60)))
 done
