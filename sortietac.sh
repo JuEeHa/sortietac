3d7a6cda1d46aaa1b938ea4439efdd3a17815c59
Juhani Haverinen <juhani.haverinen@gmail.com>
2013-08-30 16:21:22 +0300

    Added filtering support

--- sortietac.sh
+++ sortietac.sh
@@ -3,7 +3,7 @@
 echo ':j #osdev-offtopic'
 while true
 do
-	MAXLEN=$(grep -c '^..:.. <.sortiecat>' ~/irclogs/freenode/#osdev-offtopic.log)
-	grep '^..:.. <.sortiecat>' ~/irclogs/freenode/#osdev-offtopic.log | tail -n $(($RANDOM%$MAXLEN)) | head -n 1 | cut -d '>' -f 2- | cut -d ' ' -f 2-
+	MAXLEN=$(grep '^..:.. <.sortiecat>' ~/irclogs/freenode/#osdev-offtopic.log | grep -vi "$(cat secret)" | wc -l)
+	grep '^..:.. <.sortiecat>' ~/irclogs/freenode/#osdev-offtopic.log | grep -vi "$(cat secret)" | tail -n $(($RANDOM%$MAXLEN)) | head -n 1 | cut -d '>' -f 2- | cut -d ' ' -f 2-
 	sleep $(($RANDOM%(5*60)))
 done
