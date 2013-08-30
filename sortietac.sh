ea19ec528ece60e690a76a3be8d102cd43e42de9
Juhani Haverinen <juhani.haverinen@gmail.com>
2013-08-30 17:01:37 +0300

    Sortiecat practicaly rewrote it and I fixed it

--- sortietac.sh
+++ sortietac.sh
@@ -1,9 +1,33 @@
 #!/bin/sh
 
-echo ':j #osdev-offtopic'
-while true
-do
-	MAXLEN=$(grep '^..:.. <.sortiecat>' ~/irclogs/freenode/#osdev-offtopic.log | grep -vi "$(cat secret)" | wc -l)
-	grep '^..:.. <.sortiecat>' ~/irclogs/freenode/#osdev-offtopic.log | grep -vi "$(cat secret)" | tail -n $(($RANDOM%$MAXLEN)) | head -n 1 | cut -d '>' -f 2- | cut -d ' ' -f 2-
-	sleep $(($RANDOM%(5*60)))
+LOGFILE="$HOME/irclogs/freenode/#osdev-offtopic.log"
+
+function join_channel() {
+  echo ":j $1"
+}
+
+function pick_random_line() {(
+  TMP=$(mktemp)
+  grep -vi "$(cat secret)" > "$TMP"
+  NUM_LINES=$(wc -l -- "$TMP" | cut -d ' ' -f 1)
+  tail -n $(($RANDOM%$NUM_LINES)) -- "$TMP" | head -n 1
+  rm -f -- "$TMP"
+)}
+
+function say_something_sortiecat_would_say()
+{
+  grep '^..:.. <.sortiecat>' -- "$LOGFILE" | \
+  pick_random_line |  
+  cut -d '>' -f 2- | \
+  cut -d ' ' -f 2-
+}
+
+join_channel '#osdev-offtopic'
+
+(
+  (while sleep $(($RANDOM%(5*60))); do echo random_time_passed; done) &
+) | while read EVENT; do
+  case "$EVENT" in
+    random_time_passed) say_something_sortiecat_would_say ;;
+  esac
 done
